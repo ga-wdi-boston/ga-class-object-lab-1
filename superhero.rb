f614ca45842c3523747789a41c2383168221d0d5
Amal Hussein <amalhussein08@gmail.com>
2013-09-26 16:19:41 -0400

    first commit for superman...need to debug

--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,43 @@
-# Code for Superhero goes here
+class Human
+	attr_accessor :name , :gender, :age 
+
+	def initialize(name, gender, age)
+		@name = name
+		@gender = gender
+		@age = age 
+    end
+end
+
+class Superhero < Human  
+
+def fly
+	 	puts "I can fly!"
+	 end
+
+	def name 
+		 "Hello, I'm an adult my name is #{name}"
+	end 
+
+	def age 
+		"I'm #{age} years old"
+	end
+
+	def gender
+		"I'm a #{gender}"
+	end
+end
+
+batman = Superhero.new("Batman", "Superhero", 1000)
+amal = Human.new("Amal", "woman", 26)
+max = Human.new("Max", "man", 20)
+
+
+
+
+
+
+
+
+
+
+
