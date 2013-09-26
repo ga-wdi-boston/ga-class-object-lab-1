95dec358e35b9f5a228dc4a523817c6789e4e8cd
Ryan Page <rpage326@gmail.com>
2013-09-26 15:49:13 -0400

    Superheroes vs Humans

--- cars.rb
+++ cars.rb
@@ -1,1 +1,21 @@
-# Code for car class goes here
+class Human
+	def initialize(name, age, gender)
+		@name = name
+		@age = age
+		@gender = gender
+	end
+	def fly
+		"I fly so high and solo and away from u"
+	def disappear
+		"I am inViSibLE"
+	def eat
+		"I eat very QUICK"
+end
+
+h1 = Human.new
+puts a1.disappear
+puts a1.eat
+puts a1.fly
+
+
+
--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,50 @@
-# Code for Superhero goes here
+class Human
+	attr_accessor :name,
+		:age,
+		:gender
+
+	def initialize(name,age, gender)
+		@name = name
+		@age = age
+		@gender = gender
+	end
+	def walk
+		"I walk at an average speed"
+	end
+	def stand
+		"I am here standing"
+	end
+	def eat
+		"I eat at veRy average speed"
+	end
+
+end
+h1 = Human.new('Lisa Simpson', 8, 'female')
+puts h1.walk
+puts h1.stand
+puts h1.eat
+
+
+class Superhero < Human
+	def initialize(name, age, gender)
+		@name = name
+		@age = age
+		@gender = gender
+	end
+	def fly
+		"I fly so high and solo and away from u"
+	end
+	def disappear
+		"I am inViSibLE"
+	end
+	def eat
+		"I eat very QUICK"
+	end
+end
+
+s1 = Superhero.new('Quailman', 394, 'male')
+puts s1.disappear
+puts s1.eat
+puts s1.fly
+
+
