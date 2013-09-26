dc258f960cdf5a7c9cecdface454895b230cd8e7
Dan Crews <dancrews@me.com>
2013-09-26 15:57:38 -0400

    initial commit

--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,12 @@
 # Code for Superhero goes here
+class Human
+  def initialize(name, age, gender, height) 
+  	@name = name
+  	@age = age
+  	@gender = gender
+  	@height = height
+  end
+end
+
+
+I'm in the weeds on classes. 
