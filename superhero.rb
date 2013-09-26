4c8c56928c78cb57d81c1e770f38e44338e1b0e2
Emma Spencer <emma.n.spencer@gmail.com>
2013-09-26 15:49:12 -0400

    Made some classes and instantiated them

--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,65 @@
 # Code for Superhero goes here
+
+#human class
+class Human
+	attr_accessor :name, :age, :gender, :location
+
+	def initialize(name, age, gender, location)
+		@name = name
+		@age = age
+		@gender = gender
+		@location = location
+	end
+
+	def to_s
+		"I'm a regular guy."
+	end
+
+end
+
+# Superhero subclass
+class Superhero < Human
+	attr_accessor :name, :age, :gender, :location
+
+def initialize(name, age, gender, location)
+	@name = name
+	@age = age
+	@gender = gender
+	@location = location
+end
+
+	def to_s
+		"I'm a superhero!!!!"
+	end
+
+	def fly
+		"I can fly, yo."
+	end
+
+	def shoot_webs
+		"pew pew pew"
+	end
+
+	def invisibility
+		"woooo where'd I go?"
+	end
+
+	def superstrength
+		"hi-ya! Superstrong!"
+	end
+end
+
+
+pete = Human.new('Pete', 20, 'male', 'NYC')
+puts pete
+puts pete.name
+puts pete.age
+puts pete.gender
+
+
+clark = Superhero.new('Clark', 35, 'male', 'Krypton')
+puts clark
+puts clark.invisibility
+puts clark.superstrength
+puts clark.age
+puts clark.gender
