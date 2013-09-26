07ff66bd3697b827b290e43e367bda04b8937e00
Cori Allen <coriallen@gmail.com>
2013-09-26 15:49:44 -0400

    Creates code for superhero lab, half of code for cars lab.

--- cars.rb
+++ cars.rb
@@ -1,1 +1,31 @@
+# 2. Car
+
+# Define a Car class. It should have attributes like max_speed, color, seating_material, etc.
+# Define a few classes for different car makes/models
+# For example define a Ferrari class with a high max_speed
+# These classes should inherit from the Car class
+# Instantiate several different cars objects
 # Code for car class goes here
+
+class Car
+	attr_accessor :max_speed, :color, :seating_material
+
+	def initialize(max, color, seating)
+		@max_speed = max
+		@color = color
+		@seating_material = seating
+	end
+end
+
+class Ferrari
+
+	def initialize(max_speed)
+		@max_speed = max_speed
+	end
+end
+
+class Saab
+	def initialize(color)
+		@color = color
+	end
+end
--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,50 @@
+# 1. Superhero
+
+# Define a Human class. It should have attributes like name, age, gender, etc.
+# Define a Superhero module with Superhero methods like fly, shoot_web, etc.
+# Instantiate several human classes
+# Extend a few of them with Superhero methods
 # Code for Superhero goes here
+
+class Human
+	attr_accessor :name, :age, :gender
+	def initialize(name, age, gender)
+		@name = name
+		@age = age
+		@gender = gender
+	end
+
+end
+
+class Superhero < Human	
+	
+	def fly
+		puts " \
+			>=====>
+			   /"
+	end
+	
+	def shoot_web
+		puts ">>>>>>>>>>>>>>>>>>>>>>>>>web!"
+	end
+	def patriotism
+		puts "Waves flag!"
+	end
+end
+
+class Evil_Villain < Human
+	def crush_hopes_and_dreams
+		puts "you will fail and live alone with 10,000 cats!"
+	end
+end
+
+p1 = Human.new("Cynthia", 34, "lady")
+p2 = Evil_Villain.new("Carolinius", 15, "ambiguous")
+p3 = Superhero.new("Shanely", 28, "undetermined")
+p4 = Superhero.new("Crandall", 99, "man")
+
+
+puts p1.age
+puts p2.crush_hopes_and_dreams
+puts p3.name
+puts p4.patriotism
