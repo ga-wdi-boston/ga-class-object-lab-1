f5f9bf1c82eb775d3827aca7891ca973c55cb0c6
Rahul Horé <hore.rahul@gmail.com>
2013-09-26 20:06:31 -0400

    Progess on exercise 2

--- cars.rb
+++ cars.rb
@@ -1,1 +1,19 @@
-# Code for car class goes here
+class Car
+  attr_accessor :color, :seating_material
+  attr_reader   :max_speed
+
+  def initialize(max_speed, color, seating_material)
+    @max_speed = max_speed
+    @color = color
+    @seating_material = seating_material
+  end
+end
+
+class Tesla < Car
+  @max_speed = 130.0
+end
+  
+class Yugo < Car
+  @max_speed = 86.0
+end
+
