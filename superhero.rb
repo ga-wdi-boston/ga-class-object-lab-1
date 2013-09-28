53e0816a6156f015f74d13510893656e35dfa17d
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-28 10:25:25 -0400

    I have changed the 'puts' within the superhero module to returns.

--- superhero.rb
+++ superhero.rb
@@ -1,9 +1,7 @@
 # Code for Superhero goes here
-
+require 'pry'
 class Human
-	attr_accessor :name,
-		:age,
-		:gender
+	attr_accessor :name, :age, :gender
 	def initialize(name, age, gender)
 		@name = name
 		@age = age
@@ -12,32 +10,34 @@ class Human
 
 end
 
-h1 = Human.new('Hellen',23,'female')
-puts h1.name
 
-class Superhero < Human
-	def fly
-		puts "I am flying!!"
+
+module SuperHero
+	class Superhero < Human
+		def fly
+			return "I am flying!!"
+		end
+
+		def shoot_web
+			return "I am murdering the internet"
+		end
+
+		def breathe_jello
+			return "I am breathing jello!"
+		end
 	end
-
-	def shoot_web
-		puts "I am murdering the internet"
-	end
-
-	def breathe_jello
-		puts "I am breathing jello!"
-	end
-
-end
-
-class Muggle < Human
-
-end
-
-class Wizard < Human
+	binding.pry
 end
 
 
-m1 = Muggle.new('Haggard', 80,'male')
-puts m1.name
-puts m1.shoot_web
+# class Muggle < Human
+
+# end
+
+# class Wizard < Human
+# end
+
+
+# m1 = Muggle.new('Haggard', 80,'male')
+# puts m1.name
+# puts m1.shoot_web
