40c00e531a6283bd328fa2624e6e01b4cb821a5a
ChrisSherlin <rhymeswithmerlin@gmail.com>
2013-09-28 12:52:03 -0400

    I have created a working program that contains Cars and two subclasses. I need help in finding out how to initialize the max_speed, color and material for the subclasses that have their own initialized vars

--- cars.rb
+++ cars.rb
@@ -3,6 +3,7 @@
 # For example define a Ferrari class with a high max_speed
 # These classes should inherit from the Car class
 # Instantiate several different cars objects
+require 'pry'
 class Car
 	attr_accessor :max_speed, :color, :seating_material
 
@@ -13,3 +14,28 @@ class Car
 	end
 
 end
+
+
+class Honda < Car
+	attr_accessor :model, :year
+
+	def initialize(model, year)
+		@model = model
+		@year = year
+	end
+
+end
+
+class Ferrari < Car
+	attr_accessor :model, :year
+
+	def initialize(model, year)
+		@model = model
+		@year = year
+	end
+#HELP! Why does the max speed for any ferraris I create return nil?
+	
+end
+
+car1 = Honda.new("CR-C","2005")
+car2 = Ferrari.new("Model-1","2010")
