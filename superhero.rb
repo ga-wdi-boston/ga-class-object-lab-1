c346436f255ac56ab4000ff16778c8b3be02424d
Dan Crews <dancrews@me.com>
2013-09-28 13:44:41 -0400

    Commited superhero.rb

--- superhero.rb
+++ superhero.rb
@@ -1,6 +1,32 @@
-# Code for Superhero goes here
+# 1. Superhero
+# - Define a Human class.  It should have attributes like name, age, gender, etc.
+# - Define a Superhero module with Superhero methods like fly, shoot_web, etc.
+# - Instantiate several human classes
+# - Extend a few of them with Superhero methods
+
+
+module Superhero
+
+    def fly
+      "I can fly!"
+    end
+
+    def transport
+      "I can transport myself anywhere!"
+    end
+
+    def xray_vision
+      "You're not wearing any underwear!"
+    end
+
+end
+
 class Human
-  def initialize(name, age, gender, height) 
+    include Superhero
+
+  attr_accessor :name,  :age, :gender, :height
+
+  def initialize(name, age, gender, height)
   	@name = name
   	@age = age
   	@gender = gender
@@ -8,5 +34,15 @@ class Human
   end
 end
 
+joe_blow = Human.new( "Joe", 33, "male", 6)
+zimmer_man = Human.new("Bob", 74, "male", 5.5)
 
-I'm in the weeds on classes. 
+puts joe_blow.age
+
+puts joe_blow.transport
+
+puts zimmer_man.xray_vision
+
+
+
+
