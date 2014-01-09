6641c7c58076a98415cf27d741663c7858101563
Laure Fischer <laurefischer27@gmail.com>
2014-01-09 16:32:37 -0500

    updated superhero and car files

--- cars.rb
+++ cars.rb
@@ -1,1 +1,26 @@
 # Code for car class goes here
+require 'pry'
+
+class Car
+  attr_accessor :max_speed, :color, :horsepower
+
+  def initialize(max_speed, color)
+    @max_speed = max_speed
+    @color = color
+    @horsepower = 300
+  end
+end
+
+class Ferrari < Car
+  def initialize(max_speed=150, color)
+    @max_speed = max_speed
+    @color = color
+    @horsepower = 400
+  end
+end
+
+dream_car = Ferrari.new("Red")
+
+my_car = Car.new(100, "Silver")
+
+binding.pry
--- superhero.rb
+++ superhero.rb
@@ -1,1 +1,36 @@
 # Code for Superhero goes here
+require 'pry'
+
+class Human
+  attr_accessor :name, :age, :gender, :height, :weight
+
+  def initialize(name, age)
+    @name = name
+    @age = age
+    @gender = gender
+    @height = height
+    @weight = weight
+  end
+end
+
+class Superhero < Human
+  attr_accessor :cape_color
+
+  def fly
+    puts "woosh"
+  end
+
+  def shoot_web
+    puts "gotcha!"
+  end
+end
+
+me = Human.new("Laure", 24)
+
+brother = Human.new("Jeff", 21)
+
+superman = Superhero.new("Superman", 50)
+
+batman = Superhero.new("Batman", 45)
+
+binding.pry
